63df12f2fbc554e0e580dc5b198021a84b8826ee
JohnRaven <John-B-RAven@yandex.ru>
2019-10-10 00:43:06 +0300

    feat: add function (not complete)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function multiply(first, second) {
-  // your solution
-}
+/*module.exports = function multiply(first, second) {
+  first = parseInt(first, 10);
+  second = parseInt(second, 10);
+  var z = first * second;
+  let str = '' + z;
+  return str;
+}*/
+
+  var first='345721';
+  var second='359';
+  var arrX=first.split('').reverse();
+  var arrY=second.split('').reverse();
+  var arrZ=[0,0,0,0,0,0];
+
+  for (let i=0; i<arrY.length; i++) {
+    for (let j=0; j<arrX.length; j++) {
+      let z=arrY[i]*arrX[j];
+      arrZ[j]=arrZ[j]+z;
+      console.log(arrY[i],arrX[j],z,arrZ);
+
+    }
+  }
+  console.log(arrX,arrY);
+  console.log(arrZ);
+
